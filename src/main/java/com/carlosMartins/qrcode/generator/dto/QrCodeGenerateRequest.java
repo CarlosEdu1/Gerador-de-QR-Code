38f2295816ef879638fb25edaa9fd4ab4cea0183
CarlosEdu1 <carloseedu1@outlook.com>
2026-05-17 23:13:12 -0300

    first commit the project

---
+++ src/main/java/com/carlosMartins/qrcode/generator/dto/QrCodeGenerateRequest.java
@@ -0,0 +1,4 @@
+package com.carlosMartins.qrcode.generator.dto;
+
+public record QrCodeGenerateRequest(String text) {
+}
---
+++ src/main/java/com/carlosMartins/qrcode/generator/dto/QrCodeGenerateResponse.java
@@ -0,0 +1,4 @@
+package com.carlosMartins.qrcode.generator.dto;
+
+public record QrCodeGenerateResponse(String url) {
+}
